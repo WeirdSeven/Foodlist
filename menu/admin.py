d93ec7a1df236ee848ea3d2b30b176bf282e5e32
Hao Bai <hb4vf@virginia.edu>
2018-08-07 17:08:40 +0800

    New models

--- menu/admin.py
+++ menu/admin.py
@@ -1,27 +1,83 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Ingredient, Dish, Dish2Ingredient, Program, Program2Dish
-
-class Dish2IngredientInline(admin.TabularInline):
-	model = Dish2Ingredient
-	extra = 1
+from .models import *
 
 class IngredientAdmin(admin.ModelAdmin):
 	#inlines = (Dish2IngredientInline,)
 	exclude = ('ratio', )
+	ordering = ['name']
+	search_fields = ['name']
+
+class Dish2IngredientInline(admin.TabularInline):
+	model = Dish2Ingredient
+	autocomplete_fields = ['ingredient']
+	extra = 1
 	
 class DishAdmin(admin.ModelAdmin):
-	inlines = (Dish2IngredientInline,)
+	inlines = [Dish2IngredientInline]
+	search_fields = ['name']
 
 class Program2DishInline(admin.TabularInline):
 	model = Program2Dish
+	autocomplete_fields = ['dish']
+	extra = 1
+
+class CongeeSoup2IngredientInline(admin.TabularInline):
+	model = CongeeSoup2Ingredient
+	autocomplete_fields = ['ingredient']
+	extra = 1
+
+class CongeeSoupAdmin(admin.ModelAdmin):
+	inlines = [CongeeSoup2IngredientInline]
+	search_fields = ['name']
+
+class Program2CongeeSoupInline(admin.TabularInline):
+	model = Program2CongeeSoup
+	autocomplete_fields = ['congeesoup']
+	extra = 1
+
+class StapleAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+class Program2StapleInline(admin.TabularInline):
+	model = Program2Staple
+	autocomplete_fields = ['staple']
+	extra = 1
+
+class OilAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+class Program2OilInline(admin.TabularInline):
+	model = Program2Oil
+	autocomplete_fields = ['oil']
+	extra = 1
+
+class DisposableAdmin(admin.ModelAdmin):
+	search_fields = ['name']
+
+class Program2DisposableInline(admin.TabularInline):
+	model = Program2Disposable
+	autocomplete_fields = ['disposable']
 	extra = 1
 
 class ProgramAdmin(admin.ModelAdmin):
-	inlines = (Program2DishInline,)
+	inlines = [
+		Program2DishInline, 
+		Program2CongeeSoupInline, 
+		Program2StapleInline,
+		Program2OilInline, 
+		Program2DisposableInline,
+	]
 
+class SuperProgramAdmin(admin.ModelAdmin):
+	pass
 
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Dish, DishAdmin)
+admin.site.register(CongeeSoup, CongeeSoupAdmin)
+admin.site.register(Staple, StapleAdmin)
+admin.site.register(Oil, OilAdmin)
+admin.site.register(Disposable, DisposableAdmin)
 admin.site.register(Program, ProgramAdmin)
+admin.site.register(SuperProgram, SuperProgramAdmin)
--- menu/models.py
+++ menu/models.py
@@ -3,15 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 import datetime
 
-# Create your models here.
 class Ingredient(models.Model):
 	name = models.CharField(max_length = 200, unique = True, verbose_name = '配料名称')
 	ratio = models.FloatField(default = 1, verbose_name = '转化率')
 	price = models.FloatField(verbose_name = '单价(元/斤)')
 
 	class Meta:
-		verbose_name = '配料'
-		verbose_name_plural = '配料'
+		verbose_name = '配菜'
+		verbose_name_plural = '配菜'
 
 	def __str__(self):
 		return self.name
@@ -29,20 +28,105 @@ class Dish(models.Model):
 
 class Dish2Ingredient(models.Model):
 	dish = models.ForeignKey(Dish, on_delete = models.CASCADE, verbose_name = '菜品名称')
-	ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE, verbose_name = '配料名称')
+	ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE, verbose_name = '配菜名称')
 	quantity = models.FloatField(verbose_name = '重量')
 
 	class Meta:
-		verbose_name = '菜品的配料'
-		verbose_name_plural = '菜品的配料'
+		verbose_name = '菜品的配菜'
+		verbose_name_plural = '菜品的配菜'
 
 	def __str__(self):
-		return '%s %s %d斤' % (str(self.dish), str(self.ingredient), self.quantity)
+		return '%s %s %d' % (str(self.dish), str(self.ingredient), self.quantity)
+
+class CongeeSoup(models.Model):
+	name = models.CharField(max_length = 200, unique = True, verbose_name = '汤粥名称')
+	ingredient = models.ManyToManyField(Ingredient, through = 'CongeeSoup2Ingredient')
+
+	class Meta:
+		verbose_name = '汤粥'
+		verbose_name_plural = '汤粥'
+
+	def __str__(self):
+		return self.name
+
+class CongeeSoup2Ingredient(models.Model):
+	congeesoup = models.ForeignKey(CongeeSoup, on_delete = models.CASCADE, verbose_name = '汤粥名称')
+	ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE, verbose_name = '配菜名称')
+	quantity = models.FloatField(verbose_name = '重量')
+
+	class Meta:
+		verbose_name = '汤粥的配菜'
+		verbose_name_plural = '汤粥的配菜'
+
+	def __str__(self):
+		return '%s %s %d' % (str(self.congeesoup), str(self.ingredient), self.quantity)
+
+class Staple(models.Model):
+	name = models.CharField(max_length = 200, unique = True, verbose_name = '主食名称')
+	price = models.FloatField()
+
+	class Meta:
+		verbose_name = '主食'
+		verbose_name_plural = '主食'
+
+	def __str__(self):
+		return self.name
+
+class Oil(models.Model):
+	name = models.CharField(max_length = 200, unique = True, verbose_name = '油名称')
+	price = models.FloatField()
+
+	class Meta:
+		verbose_name = '油'
+		verbose_name_plural = '油'
+
+	def __str__(self):
+		return self.name
+
+class Condiment(models.Model):
+	name = models.CharField(max_length = 200, unique = True, verbose_name = '调料名称')
+	price = models.FloatField()
+
+	class Meta:
+		verbose_name = '调料'
+		verbose_name_plural = '调料'
+
+	def __str__(self):
+		return self.name
+
+class Disposable(models.Model):
+	name = models.CharField(max_length = 200, unique = True, verbose_name = '一次性用品名称')
+	price = models.FloatField()
+
+	class Meta:
+		verbose_name = '一次性用品'
+		verbose_name_plural = '一次性用品'
+
+	def __str__(self):
+		return self.name
+
+
+class SuperProgram(models.Model):
+	name = models.CharField(max_length = 200, unique = True, verbose_name = '大项目名称')
+
+	class Meta:
+		verbose_name = '大项目'
+		verbose_name_plural = '大项目'
+
+	def __str__(self):
+		return self.name
 
 class Program(models.Model):
 	name = models.CharField(max_length = 200, unique = True, verbose_name = '项目名称')
+	superprogram = models.ForeignKey(SuperProgram, null = True, on_delete = models.SET_NULL, verbose_name = '大项目名称')
 	date = models.DateField(default = datetime.date.today, verbose_name = '日期')
+
 	dishes = models.ManyToManyField(Dish, through = 'Program2Dish')
+	congeesoups = models.ManyToManyField(CongeeSoup, through = 'Program2CongeeSoup')
+	staples = models.ManyToManyField(Staple, through = 'Program2Staple')
+	condiments = models.FloatField(verbose_name = '项目的调料')
+	oil = models.ManyToManyField(Oil, through = 'Program2Oil')
+	disposables = models.ManyToManyField(Disposable, through = 'Program2Disposable')
 
 	class Meta:
 		verbose_name = '项目'
@@ -62,3 +146,64 @@ class Program2Dish(models.Model):
 
 	def __str__(self):
 		return '%s %s %d' % (str(self.program), str(self.dish), self.count)
+
+class Program2CongeeSoup(models.Model):
+	program = models.ForeignKey(Program, on_delete = models.CASCADE, verbose_name = '项目名称')
+	congeesoup = models.ForeignKey(CongeeSoup, on_delete = models.CASCADE, verbose_name = '汤粥名称')
+	count = models.IntegerField(verbose_name = '份数')
+
+	class Meta:
+		verbose_name = '项目的汤粥'
+		verbose_name_plural = '项目的汤粥'
+
+	def __str__(self):
+		return '%s %s %d' % (str(self.program), str(self.congeesoup), self.count)
+
+class Program2Staple(models.Model):
+	program = models.ForeignKey(Program, on_delete = models.CASCADE, verbose_name = '项目名称')
+	staple = models.ForeignKey(Staple, on_delete = models.CASCADE, verbose_name = '主食名称')
+	count = models.IntegerField(verbose_name = '用量')
+
+	class Meta:
+		verbose_name = '项目的主食'
+		verbose_name_plural = '项目的主食'
+
+	def __str__(self):
+		return '%s %s %d' % (str(self.program), str(self.staple), self.count)
+
+class Program2Oil(models.Model):
+	program = models.ForeignKey(Program, on_delete = models.CASCADE, verbose_name = '项目名称')
+	oil = models.ForeignKey(Oil, on_delete = models.CASCADE, verbose_name = '油名称')
+	count = models.IntegerField(verbose_name = '用量')
+
+	class Meta:
+		verbose_name = '项目的油'
+		verbose_name_plural = '项目的油'
+
+	def __str__(self):
+		return '%s %s %d' % (str(self.program), str(self.oil), self.count)
+
+class Program2Condiment(models.Model):
+	program = models.ForeignKey(Program, on_delete = models.CASCADE, verbose_name = '项目名称')
+	condiment = models.ForeignKey(Condiment, on_delete = models.CASCADE, verbose_name = '调料名称')
+	count = models.IntegerField(verbose_name = '用量')
+
+	class Meta:
+		verbose_name = '项目的调料'
+		verbose_name_plural = '项目的调料'
+
+	def __str__(self):
+		return '%s %s %d' % (str(self.program), str(self.condiment), self.count)
+
+class Program2Disposable(models.Model):
+	program = models.ForeignKey(Program, on_delete = models.CASCADE, verbose_name = '项目名称')
+	disposable = models.ForeignKey(Disposable, on_delete = models.CASCADE, verbose_name = '一次性用品名称')
+	count = models.IntegerField(verbose_name = '用量')
+
+	class Meta:
+		verbose_name = '项目的一次性用品'
+		verbose_name_plural = '项目的一次性用品'
+
+	def __str__(self):
+		return '%s %s %d' % (str(self.program), str(self.disposable), self.count)
+
