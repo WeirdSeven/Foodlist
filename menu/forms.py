ac6926918cbfaed232d6cb4fc29f55c2155f1f4d
Hao Bai <haobai26@gmail.com>
2021-10-13 13:57:48 +0800

    Reformat some files

--- menu/forms.py
+++ menu/forms.py
@@ -3,32 +3,35 @@
 from django import forms
 import datetime
 
+
 class IngredientForm(forms.Form):
-	name = forms.CharField(label = '名称')
-	price = forms.FloatField(label = '价格（单位：元/克）')
-	ratio = forms.FloatField(label = '转化率')
+    name = forms.CharField(label='名称')
+    price = forms.FloatField(label='价格（单位：元/克）')
+    ratio = forms.FloatField(label='转化率')
+
 
 class Dish2IngredientForm(forms.Form):
-	ingredient_name = forms.CharField(widget=forms.TextInput(attrs={
-									'placeholder': '原料名称',
-									}))
-	quantity = forms.FloatField()
+    ingredient_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '原料名称',
+    }))
+    quantity = forms.FloatField()
+
 
 class DishForm(forms.Form):
-	dish_name = forms.CharField(widget=forms.TextInput(attrs={
-								  'placeholder': '菜品名称',
-								  }), label = '菜品名称')
+    dish_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '菜品名称',
+    }), label='菜品名称')
+
 
 class ProgramForm(forms.Form):
-	program_name = forms.CharField(widget=forms.TextInput(attrs={
-								  'placeholder': '项目名称',
-								  }), label = '项目名称')
-	date = forms.DateField(widget = forms.SelectDateWidget, initial = datetime.date.today, label = '日期')
+    program_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '项目名称',
+    }), label='项目名称')
+    date = forms.DateField(widget=forms.SelectDateWidget, initial=datetime.date.today, label='日期')
 
 
 class Program2DishForm(forms.Form):
-	dish_name = forms.CharField(widget=forms.TextInput(attrs={
-									'placeholder': '菜品名称',
-									}))
-	count = forms.IntegerField()
-
+    dish_name = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '菜品名称',
+    }))
+    count = forms.IntegerField()
--- menu/urls.py
+++ menu/urls.py
@@ -4,14 +4,23 @@ from . import views
 
 app_name = 'menu'
 urlpatterns = [
-	path('', views.index.index, name = 'index'),
-	path('dish/', views.dish.dish, name = 'dish'),
-	path('ingredient/', views.ingredient.ingredient, name = 'ingredient'),
-	path('program/', views.program.program, name = 'program'),
+    path('', views.index.index, name='index'),
+    path('dish/', views.dish.dish, name='dish'),
+    path('ingredient/', views.ingredient.ingredient, name='ingredient'),
+    path('program/', views.program.program, name='program'),
 
-
-	path('reports/', views.reports.report_list, name = 'reports'),
-	path('reports/<int:year>/<int:month>/<int:day>/', views.reports.report, name = 'daily_reports'),
-	path('reports/<int:year>/<int:month>/<int:day>/program/<program>', views.reports.report, {'is_super_program': False}, name='program_reports'),
-	path('reports/<int:year>/<int:month>/<int:day>/super_program/<program>', views.reports.report, {'is_super_program': True}, name='super_program_reports'),
+    path('reports/', views.reports.report_list, name='reports'),
+    path('reports/<int:year>/<int:month>/<int:day>/', views.reports.report, name='daily_reports'),
+    path(
+        'reports/<int:year>/<int:month>/<int:day>/program/<program>',
+        views.reports.report,
+        {'is_super_program': False},
+        name='program_reports'
+    ),
+    path(
+        'reports/<int:year>/<int:month>/<int:day>/super_program/<program>',
+        views.reports.report,
+        {'is_super_program': True},
+        name='super_program_reports'
+    ),
 ]
