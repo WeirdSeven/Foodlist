db3873edcb1e3b06c4eb3efdaf7f9e350177520c
Hao Bai <haobai26@gmail.com>
2021-12-01 17:11:03 +0800

    Switch to PostgreSQL

--- Foodlist/settings.py
+++ Foodlist/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'Foodlist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'haobai',
+        'PASSWORD': 'hb123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
